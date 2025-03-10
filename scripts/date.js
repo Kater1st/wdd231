2272379e7b46bb1e462cec0959c67e23667fe69f
Kater1st <kat24007@byui.edu>
2025-03-11 00:15:09 +0200

    assignment done

---
+++ scripts/date.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const yearSpan = document.getElementById('currentyear');
+    yearSpan.textContent = new Date().getFullYear();
+    
+    const lastModified = document.getElementById('lastModified');
+    lastModified.textContent = `Last Modified: ${document.lastModified}`;
+});
